52b9007fe1a6a5dc2cff0c32cbaa5842a9298246
IonaLn <antonio.fon.salieri@gmail.com>
2024-11-04 20:03:43 +0200

    body version 0.1

--- body.py
+++ body.py
@@ -1,2 +1,25 @@
-Popinka <3
-print(pupunia)
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+number_pressed = 0
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/click', methods=['POST'])
+def button_pressed():
+    global number_pressed
+    data = request.json
+
+    clicked = data.get("clicked", False)
+
+    if clicked:
+        click_count += 1
+        print("button pressed ", click_count)
+        
+    return jsonify({"status": "1"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
